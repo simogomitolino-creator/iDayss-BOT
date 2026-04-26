dcb47234f1fb239ada668a3df548e53be51bfd8c
simogomitolino-creator <simogomitolino@gmail.com>
2026-04-26 10:28:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.commands = new Collection();
+client.eventModules = new Map();
+
+// Carica comandi
+const commandsPath = path.join(__dirname, 'commands');
+for (const file of fs.readdirSync(commandsPath).filter(f => f.endsWith('.js'))) {
+  const command = require(path.join(commandsPath, file));
+  if (command.data && command.execute) {
+    client.commands.set(command.data.name, command);
+    console.log('Comando caricato: ' + command.data.name);
+  }
+}
+
+// Carica eventi
+const eventsPath = path.join(__dirname, 'events');
+for (const file of fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'))) {
+  const event = require(path.join(eventsPath, file));
+  // Salva il modulo nella mappa per condivisione dati tra eventi
+  client.eventModules.set(event.name, event);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+  console.log('Evento caricato: ' + event.name);
+}
+
+client.login(process.env.TOKEN);
